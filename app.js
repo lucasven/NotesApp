e72ee445c9ad23eb5f0c8ea1cd46848f0bf78b5e
Lucas Venturella <lucas.venturella@getambush.com>
2021-05-26 15:40:13 -0300

    playing around with yargs to do a cli

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
-const getNotes = require('./notes.js');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-const command = process.argv[2];
+const getNotes = require('./notes.js');
 
-if(command === 'add') {
-    console.log('Adding note!');
-} else if(command === 'remove'){
-    console.log('Removing note!');
-}
+// Customize yargs version
+yargs.version('1.1.0');
+
+// create add comment
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Command body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ', argv.title);
+        console.log('Body: ', argv.body);
+    }
+})
+
+// create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note');
+    }
+})
+
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function() {
+        console.log('Listing the notes');
+    }
+})
+
+// create list command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading the note');
+    }
+})
+
+yargs.parse();
